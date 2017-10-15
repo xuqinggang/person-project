a032957179135a4b1f0e65167a41f5e5d88854b7
许庆钢 <xuqinggang@xuqinggangs-MacBook-Air.local>
2017-10-15 19:24:56 +0800

    update app/index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,19 @@
-ureStore();
+import ReactDOM from 'react-dom';
+import React from 'react';
+import configureStore from './redux/configureStore';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import routes from './routes';
+// import DevTools from './redux/DevTools';
+import injectTapEventPlugin from "react-tap-event-plugin";
+import TestMenu from './views/Test/kzWeb/Menu/TestMenu';
+import Menu, { MenuItem, SubMenu } from './views/Test/kzWeb/Menu';
+// import Menu, {SubMenu, MenuItem} from 'rc-menu';
+// 导入重置样式
+import './styles/index.scss'
+
+const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
 injectTapEventPlugin();
